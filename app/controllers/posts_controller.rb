5472318fd35c5f80e4e30a9ac1bec2a6cd7ea575
ToanDK <khactoan96@gmail.com>
2017-11-21 00:40:10 +0700

    Edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, except: %i(index new create)
+  before_action :authenticate_user!, except: %i(index show)
 
   def index
     @posts = Post.paginate :page => params[:page], :per_page => Settings.per_page
